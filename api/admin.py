6b2c92b903e760baba9f340f1a24a3480028bb03
Kamil Marut <kamilmarut@protonmail.com>
2021-01-26 12:09:33 +0100

    Register admins for models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Menu, Dish
+
+
+class BaseAdmin(admin.ModelAdmin):
+    readonly_fields = ()
+
+    def get_readonly_fields(self, request, obj=None):
+        base_fields = ["created_at", "updated_at"]
+        return set(list(self.readonly_fields) + base_fields)
+
+
+class MenuAdmin(BaseAdmin):
+    model = Menu
+    fields = (
+        "name",
+        "description",
+        "dishes",
+    )
+
+
+class DishAdmin(BaseAdmin):
+    model = Dish
+    fields = (
+        "name",
+        "description",
+        "price",
+        "image",
+        "preparation_time",
+        "is_vegetarian",
+    )
+    list_display = (
+        "name",
+        "price",
+        "preparation_time",
+        "is_vegetarian",
+    )
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Dish, DishAdmin)
